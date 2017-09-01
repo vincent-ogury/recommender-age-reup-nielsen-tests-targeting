cb5bc155ff2664decce961b661b3098bb08aa9c3
Vincent Bachelier <vincent@ogury.co>
2017-09-01 16:35:16 +0200

    bash to upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+time python splitter.py
+
+aws s3 rm --recursive s3://ogury-tmp/vincent/recommender-age/results/
+aws s3 cp --recursive results/ s3://ogury-tmp/vincent/recommender-age/results/
+
+for i in `seq 0 30`
+do
+  [ "$(date -j -v+${i}d +%m)" != "09" ] && break
+  d=$(date -j -v+${i}d +%Y-%m-%d)
+  echo aws s3 cp --recursive s3://ogury-tmp/vincent/recommender-age/results/ s3://ogury-ods/prod/users_external/dt=${d}/
+done | parallel -u
+
