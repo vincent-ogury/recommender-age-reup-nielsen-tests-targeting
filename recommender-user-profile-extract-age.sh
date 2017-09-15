aa0c37f52a36db21c95dac2e1c97c8128e0f4a8b
Vincent Bachelier <vincent@ogury.co>
2017-09-15 16:31:22 +0200

    use real training

--- recommender-user-profile-extract-age.sh
+++ recommender-user-profile-extract-age.sh
@@ -1,6 +1,7 @@
-aws s3 sync s3://ogury-tmp/vincent/recommender-user-profile/data/ recommender-user-profile
-aws s3 rm --rec s3://ogury-tmp/vincent/recommender-user-profile/
-cd recommender-user-profile
+#aws s3 sync s3://ogury-tmp/vincent/recommender-user-profile/data/ recommender-user-profile
+#aws s3 rm --rec s3://ogury-tmp/vincent/recommender-user-profile/
+aws s3 sync s3://ogury-recommender-prod/recommender-user-profile/data/ recommender-user-profile/training/
+cd recommender-user-profile/training/
 for i in dt\=????-??-??
 do
   if [ ! -f "$i".csv ]
